ca83fb78a9d5b15658abff3245e9784cd2a909b5
Shikuku39 <enshikuku39@gmail.com>
2024-06-18 11:47:48 +0300

    Add variable declarations in TypeScript

---
+++ 01_basics/02_variables.ts
@@ -0,0 +1,9 @@
+// Variable declarations
+let message: string = "Hello, World";
+let count: number = 42;
+let isDone: boolean = true;
+
+
+console.log(message);
+console.log(count);
+console.log(isDone);
